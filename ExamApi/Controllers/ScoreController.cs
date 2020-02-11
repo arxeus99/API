91b07bf6dac396daf8043207c219f5203f460292
arxeus99 <arxeus99@gmail.com>
2020-02-11 17:58:52 +0100

    first commit

---
+++ ExamApi/Controllers/ScoreController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamApi.Controllers
+{
+    public class ScoreController : Controller
+    {
+        // GET: Score
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Score/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Score/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Score/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Score/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Score/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Score/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Score/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ dbo.AspNetUsersScore.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AspNetUsersScore]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [barrels] INT NULL DEFAULT 0, 
+    [balls] INT NULL DEFAULT 0, 
+    [pizzas] INT NULL DEFAULT 0, 
+    [cookies] INT NULL DEFAULT 0, 
+    [skulls] INT NULL DEFAULT 0
+)
